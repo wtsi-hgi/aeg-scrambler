b02f7feefdc354d2f67caa47e4d4852c89d780e2
Ronnie-Crawford <rc30@sanger.ac.uk>
2023-05-05 16:18:24 +0100

    Added metrics class

--- aeg_scrambler/gene_expression.py
+++ aeg_scrambler/gene_expression.py
@@ -34,51 +34,56 @@ class GeneExpression:
         # interest's expression for each gene, dropping irrelevant columns,
         # and dropping duplicate genes
 
-        self.data = self.data.drop(["depmapID", "primary_disease"], axis = 0)
-        self.data.columns = self.data.iloc[0]
-        self.data = self.data.iloc[1:]
-        self.data = self.data.reset_index() \
+        self.general_data = self.general_data \
+            .drop(["depmapID", "primary_disease"], axis = 0)
+        self.general_data.columns = self.data.iloc[0]
+        self.general_data = self.general_data.iloc[1:]
+        self.general_data = self.general_data.reset_index() \
             .rename(columns = {"index" : "Gene_name"})
-        self.data = self.data. \
+        self.general_data = self.data. \
             rename(columns = 
                    {config.cell_line_of_interest : "General_gene_expression"})
-        self.data = self.find_mean(self.data)
-        self.data = self.find_std(self.data)
-        self.data = self.find_anomalous_score_of_gene_expression(self.data)
-        self.data = self.data[["Gene_name", "General_gene_expression",
-                                    "Mean", "Std", "Anomalous_score"]]
-        self.data = self.data \
+        self.general_data = self.find_mean(self.general_data)
+        self.general_data = self.find_std(self.general_data)
+        self.general_data = self. \
+            find_anomalous_score_of_gene_expression(self.general_data)
+        self.general_data = self.general_data[["Gene_name",
+                                               "General_gene_expression",
+                                               "Mean",
+                                               "Std",
+                                               "Anomalous_score"]]
+        self.general_data = self.data \
             .drop_duplicates(keep = False, subset = ["Gene_name"])
             
-    def find_mean(df):
+    def find_mean(data):
         
         # Adds the mean of gene expression to the given expression data frame,
         # giving a mean for each gene
         
-        df["Mean"] = df.loc[:, df.columns != "Gene_name"].mean(axis = 1)
+        data["Mean"] = data.loc[:, data.columns != "Gene_name"].mean(axis = 1)
         
-        return df
+        return data
         
-    def find_std(df):
+    def find_std(data):
         
         #Adds the standard deviation to the given expression dataframe, giving the
         #standard deviation across expression of each gene in all provided cell
         #types
         
-        df["Std"] = df.loc[:, df.columns != "Gene_name"].std(axis = 1)
+        data["Std"] = data.loc[:, data.columns != "Gene_name"].std(axis = 1)
         
-        return df
+        return data
     
-    def find_anomalous_score_of_gene_expression(df):
+    def find_anomalous_score_of_gene_expression(data):
     
         # Adds the z-score of each gene based on its expression
         # in the cell line of interest compared to all others
         
-        df["Anomalous_score"] = df.apply(lambda gene :
+        data["Anomalous_score"] = data.apply(lambda gene :
             (gene["General_gene_expression"] -
              gene["Mean"]) / gene["Std"], axis = 1)
 
-        return df
+        return data
     
     def read_specific_expression_data(self, config):
     
@@ -86,11 +91,12 @@ class GeneExpression:
         # dataframe.
 
         try:
-            self.data = \
-                pd.read_csv(config.specific_expression_by_cell_line_reference_path,
-                            sep = "\t", 
-                            names = ["Gene_name", "Specific_gene_expression"],
-                            skiprows = 1)   
+            self.specific_data = \
+                pd.read_csv(config. \
+                    specific_expression_by_cell_line_reference_path,
+                    sep = "\t", 
+                    names = ["Gene_name", "Specific_gene_expression"],
+                    skiprows = 1)   
         
         except:
             print("ERROR: Specific expression data could not be read.")
@@ -101,9 +107,10 @@ class GeneExpression:
         # turning minus infinite strings into a floating point representation
         # of negative infinity, duplicate genes are dropped
         
-        self.data["Specific_gene_expression"] = \
-            self.data["Specific_gene_expression"]. \
+        self.specific_data["Specific_gene_expression"] = \
+            self.specific_data["Specific_gene_expression"]. \
                 apply(lambda expression : \
                     0 if expression == "-Inf" else pow(2, expression))
-        self.data = \
-            self.data.drop_duplicates(keep = False, subset = ["Gene_name"])
+        self.specific_data = \
+            self.specific_data. \
+                drop_duplicates(keep = False, subset = ["Gene_name"])
