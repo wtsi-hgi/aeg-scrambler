914574e7e5bafdbaaa0004808327e2e2a4c25e65
KMace <kyle.mace@hotmail.co.uk>
2023-05-09 10:09:54 +0100

    Remove Files

--- aeg_scrambler/aeg_scrambler.py
+++ aeg_scrambler/aeg_scrambler.py
@@ -3,3 +3,11 @@
 This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
+
+class GeneData:
+
+    def __init__(self) -> None:
+        """Constructor."""
+        
+        return "hello from init"
+    
