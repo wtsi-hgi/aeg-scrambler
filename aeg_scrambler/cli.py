570c0df91b6639ac7f29f5521b2423377e1cbf48
KMace <kyle.mace@hotmail.co.uk>
2023-05-16 18:23:21 +0100

    Neater + Reduced Code

--- aeg_scrambler/cli.py
+++ aeg_scrambler/cli.py
@@ -6,14 +6,13 @@ LICENSE file in the root directory of this source tree.
 
 import typer
 
-import config
-import gene_annotations
-import gene_expression
-import regulatory_element_annotations
-import metrics
-import coordinates
-import sequences
-
+from .config import Config
+from .gene_annotations import GeneAnnotations
+from .gene_expression import GeneExpression
+from .regulatory_element_annotations import RegulatoryElementAnnotations
+from .metrics import Metrics
+from .coordinates import Coordinates
+from .sequences import Sequences
 
 app = typer.Typer()
 
@@ -25,50 +24,45 @@ def configure(configuration, path):
 
 @app.command()
 def rank(configuration):
-    
     # Prioritises genes based on weights of various factors
     
-    instance_gene_annotations = gene_annotations.GeneAnnotations(configuration)
-    instance_gene_expressions = gene_expression.GeneExpression(configuration)
+    instance_gene_annotations = GeneAnnotations(configuration)
+    instance_gene_expressions = GeneExpression(configuration)
     instance_regulatory_element_annotations = \
-        regulatory_element_annotations \
-            .RegulatoryElementAnnotations(configuration)
+        RegulatoryElementAnnotations(configuration)
 
-    instance_metrics = metrics.Metrics(configuration,
-                                       instance_gene_annotations,
-                                       instance_regulatory_element_annotations,
-                                       instance_gene_expressions)
+    instance_metrics = Metrics(
+        configuration,
+        instance_gene_annotations,
+        instance_regulatory_element_annotations,
+        instance_gene_expressions
+    )
     instance_metrics.print_metrics()
 
 @app.command()
 def tune(gene, metrics):
-    
     # Tune the weights to find the local maxima rank of a specific gene
-    
-    
+    pass
 
 @app.command()
 def design(configuration, metrics):
-    
     # Taking a list of ranked genes, will design pegRNA sequences for the 
     # regions between enhancers and call PRIDICT to find the most efficient
     # in each inter-enhancer sequence
     
-    coordinates = coordinates.Coordinates(configuration, metrics)
-    sequences = sequences.Sequences(configuration, coordinates)
-    
-        
+    coordinates = Coordinates(configuration, metrics)
+    sequences = Sequences(configuration, coordinates)
+
 @app.command()
 def explore():
-    
     # Taking a list of genes will display their distribution and 
     # generate several sets of graphs for several features
     
     print("Exploring")
+    pass
 
 @app.command()
 def export():
-    
     # Export either a wig or bed file of plateau regions,
     # convolution signal etc as decided by options
 
@@ -77,7 +71,7 @@ def export():
 def main():
     
     app()
-    configuration = config.Config()
+    configuration = Config()
 
 if __name__ == "__main__":
     main()
