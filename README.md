e107a149cee174363aedee187aa98849fc88bd57
Ronnie-Crawford <rc30@sanger.ac.uk>
2023-05-31 13:14:19 +0100

    New README, new sequences, same old aeg-scrambler

--- README.md
+++ README.md
@@ -1,8 +1,176 @@
 # AEG SCRAMBLEr
 
+## Introduction
+
+### Step-by-step
+
+1. The user provides files including: 
+2. The rank command
+
 ## How to install
 
+Run the following command to install aeg-scrambler:
+
+~~~
+command --to run
+~~~
+
 ## Configuration
+
+All settings for the tool are provided by the user as a .json file. This is
+split into the following sections:
+
+### Paths
+
+The paths which the tool will look for certain files are provided:
+
+~~~
+    "results_directory" : "/my/path/here/results/",
+    "gene_report_directory" : "/my/path/here/reports/",
+    "gene_annotation_path" : "/my/path/here/gene_annotations.gtf",
+    "regulatory_elements_path" : "//my/path/here//epigenetic_flags.bed",
+    "ccle_expression_path" : "//my/path/here//CCLE_expressions_data.csv",
+    "experimental_expression_path" : "/my/path/here/experimental_expressions.tsv",
+    "hic_path" : "/my/path/here/hic_data.file",
+    "reference_genome_path" : "/my/path/here/reference.fa",
+~~~
+
+All paths must be provided as strings, directories must be followed with a
+forward slash. Absolute paths are preferred.
+
+### Biological specifications
+
+These settings are used to specify biological details of the data:
+
+~~~
+    "cell_line_of_interest" : "CELL-LINE",
+    "chromosomes_of_interest" : ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "X", "Y"],
+    "flags_of_interest" : ["FLAG1"],
+    "quiescent_epigenetic_flags_of_interest" : ["FLAG2"],
+~~~
+
+### Search window
+
+Allows the user to define the search window that searches around each gene:
+
+~~~
+    "search_type" : "whole_gene",
+    "upstream_search" : 500000,
+    "downstream_search" : 500000,
+~~~
+
+"search_type" can be either "whole_gene" (upstream and downstream are defined
+from the ends of the gene) or "start_site" (upstream and downstream are
+defined from the start site of the gene).
+"upstream search" and "downstream search" must be integers, and define the
+bases included upstrem and downstream within the search window.
+
+### Hard filters
+
+These filters are used to cut out undesired genes from any processing:
+
+~~~
+    "std_max" : false,
+    "std_min" : 1,
+    "anomalous_expression_max" : false,
+    "anomalous_expression_min" : false,
+    "enhancer_count_max" : false,
+    "enhancer_count_min" : false,
+    "enhancer_proportion_max" : false,
+    "enhancer_proportion_min" : false,
+    "cell_line_expression_max" : false,
+    "cell_line_expression_min" : false,
+    "gene_size_max" : false,
+    "gene_size_min" : false,
+    "symmetry_max" : false,
+    "symmetry_min" : false,
+~~~
+
+Filters may either be false (no filter) or a number (integer / float
+as necessary)
+
+### Weights
+
+Weights are set to decide how genes are prioritised by the "rank" function:
+
+~~~
+    "std_weight" : 0,
+    "anomalous_expression_weight" : 0.5,
+    "enhancer_count_weight" : 1,
+    "enhancer_proportion_weight" : 1.5,
+    "cell_line_expression_weight" : 2,
+    "gene_size_weight" : 2.5,
+    "symmetry_weight" : 3,
+~~~
+
+These may be integers or floats, positive, negative or zero.
+
+### Enhancer kernel parameters
+
+The following parameters define the kernel used to convole the signal of
+enhancers:
+
+~~~
+    "enhancer_kernel_shape" : "guassian",
+    "enhancer_kernel_size_type" : "relative",
+    "absolute_enhancer_kernel_size" : 1000,
+    "absolute_enhancer_kernel_sigma" : 3,
+    "relative_enhancer_kernel_size" : 0.1,
+    "relative_enhancer_kernel_sigma" : 0.005,
+~~~
+
+"enhancer_kernel_shape" may be "flat" or "guassian".
+"enhancer_kernel_size_type" may be "absolute" or "relative", and defines
+whether the absolute or relative settings are used; an absolute kernel is the
+same for each gene and the size is the number of bases wide it is, a relative
+kernel has its size and sigma relative to the gene which is being convolved.
+
+### Quiescent kernel parameters
+
+The following parameters define the kernel used to convole the signal of
+quiescent regions:
+
+~~~
+    "quiescent_kernel_shape" : "guassian",
+    "quiescent_kernel_size_type" : "relative",
+    "absolute_quiescent_kernel_size" : 1000,
+    "absolute_quiescent_kernel_sigma" : 3,
+    "relative_quiescent_kernel_size" : 0.1,
+    "relative_quiescent_kernel_sigma" : 0.0015,
+~~~
+
+"quiescent_kernel_shape" may be "flat" or "guassian".
+"quiescent_kernel_size_type" may be "absolute" or "relative", and defines
+whether the absolute or relative settings are used; an absolute kernel is the
+same for each gene and the size is the number of bases wide it is, a relative
+kernel has its size and sigma relative to the gene which is being convolved.
+
+### Interferring genes
+
+These settings define how genes that are nearby other genes affect each gene's
+search window, if a gene is defined as interferring, it will cut off a search
+window prematurely.
+
+~~~
+    "specific_expression_threshold" : 0.01,
+    "interferring_gene_overlaps" : false,
+~~~
+
+### Convolution settings
+
+~~~
+    "convolution_limit" : 2,
+    "enhancer_convolution_weight" : 1,
+    "quiescent_convolution_weight" : 1,
+    "plateau_threshold" : 0.1,
+~~~
+
+### Prime editing settings
+
+~~~
+    "inserted_sequence" : "ATAACTTCGTATAATGTACATTATACGAAGTTAT",
+    "partial_insertions_per_region" : 20,
+~~~
 
 ## Commands
 
--- aeg_scrambler/sequences.py
+++ aeg_scrambler/sequences.py
@@ -6,217 +6,22 @@ import subprocess
 class Sequences:
     
     def __init__(self, config, coordinates):
-
-
-        self.plateau_insertions = pd.DataFrame(
-            columns = [
-                "Sequence_name", 
-                "Insertion_sequence", 
-                "Insertion_location", 
-                "Plateau_sequence"
-            ])
-        self.inserted_sequence = config.inserted_sequence
-        self.partial_insertions_per_region = \
-            config.partial_insertions_per_region
-        self.data = coordinates.data
-        self.iterate_gene_plateaus(config)
-        print(self.plateaus)
-        self.run_pridict(config)
-        print(self.plateaus)
-        self.find_fasta(config)
-        print(self.plateaus)
-        self.generate_pridict_input(config)
         
-    def iterate_gene_plateaus(self, config):
-        
-        """
-        Saves plateaus associated with each gene as a bed file.
-        """
+        self.export_plateaus_for_each_gene(config)
+
+    def export_plateaus_for_each_gene(self, config):
         
         for index, gene in self.data.head(
             config.convolution_limit
         ).iterrows():
 
-            self.plateaus = pd.DataFrame(
+            plateaus = pd.DataFrame(
                 {"Start" : self.data.loc[index, "Plateau_starts"], 
                 "End" : self.data.loc[index, "Plateau_ends"]
                 }
             )
-            self.plateaus["Gene_name"] = gene["Gene_name"]
-            self.plateaus["Chromosome"] = "chr" + gene["Chromosome"]
-            self.plateaus["Strand"] = gene["Strand"]
+            plateaus["Gene_name"] = gene["Gene_name"]
+            plateaus["Chromosome"] = "chr" + gene["Chromosome"]
+            plateaus["Strand"] = gene["Strand"]
         
-    def find_fasta(self, config):
-        
-        """
-        Takes coordinates of plateaus and returns FASTA
-        sequence from reference genome
-        """
-        
-        plateaus_pr = pr.PyRanges(self.plateaus)
-        
-        seq = pr.get_sequence(plateaus_pr, config.reference_genome_path)
-        plateaus_pr.seq = seq
-        self.plateaus = plateaus_pr.df
-        self.plateaus.rename(columns = {"seq" : "Sequence"}, inplace = True)
-
-    def generate_pridict_input(self, config):
-        
-        """
-        For each plateau, finds partial insertion sites, 
-        exports sequences with insertions to csv
-        """
-        
-        self.plateaus.apply(
-            self.generate_insertion_prefixes_and_suffixes,
-            axis = 1,
-        )
-        
-        self.plateau_insertions["Sequence"] = self.plateau_insertions.apply(
-            self.insert_insertion_sequence,
-            axis = 1
-        )
-        
-        with open(
-            config.results_directory + "sequences_for_pridict.csv", "w"
-        ) as pridict_input:
-
-            pridict_input.write("sequence_name,editseq\n")
-        
-        self.plateau_insertions.to_csv(
-            (config.results_directory + "sequences_for_pridict.csv"),
-            index = False, columns = ["Sequence_name", "Sequence"],
-            mode = "a", header = False
-        )
-
-    def generate_insertion_prefixes_and_suffixes(self, plateau):
-        
-        """
-        Iteratively finds all possible partial insertions in order of length.
-        """
-        
-        plateau_insertion_sites = pd.DataFrame(
-            columns = [
-                "Sequence_name", 
-                "Insertion_sequence",
-                "Insertion_location", 
-                "Plateau_sequence"
-            ])
-        
-        for num_bases_absent in range(0, len(self.inserted_sequence)):
-            for checked_position in range(
-                0, (len(self.inserted_sequence) - num_bases_absent)):
-
-                absent_sequence = self.inserted_sequence[
-                    checked_position : (checked_position + num_bases_absent)
-                ]
-                present_sequence = (
-                    self.inserted_sequence[:checked_position] 
-                    + self.inserted_sequence[
-                        (checked_position + num_bases_absent):
-                    ]
-                )
-                insertion_positions = self.find_prefix_suffix_in_plateau(
-                    plateau,
-                    present_sequence
-                )
-                
-                for position in insertion_positions:
-                    new_row = pd.Series({
-                        "Sequence_name" : (
-                            plateau["Gene_name"] + 
-                            "_" + plateau["Chromosome"] + 
-                            "_" + plateau["Strand"] + 
-                            "_"  + str(plateau["Start"]) + 
-                            "-" + str(plateau["End"]) +
-                            "+" + str(position + checked_position) +
-                            ":" + absent_sequence
-                        ),
-                        "Insertion_sequence" : absent_sequence,
-                        "Insertion_location" : (position + checked_position),
-                        "Plateau_sequence" : plateau["Sequence"]
-                    })
-                    new_df = pd.DataFrame([new_row])
-                    plateau_insertion_sites = pd.concat(
-                        [plateau_insertion_sites, new_df],
-                        axis = 0, ignore_index = True
-                    )
-                    
-                    if len(plateau_insertion_sites.index) < self.partial_insertions_per_region:
-                        
-                        self.plateau_insertions = pd.concat(
-                            [self.plateau_insertions, plateau_insertion_sites], 
-                            axis = 0, ignore_index = True
-                        )
-
-                        break
-        
-    def find_prefix_suffix_in_plateau(self, plateau, present_sequence):
-        
-        """
-        Searches plateau sequences for partial insertion sequences
-        """
-        
-        insertions = re.finditer(
-            pattern = present_sequence, 
-            string = plateau["Sequence"]
-        )
-        insertion_positions = [index.start() for index in insertions]
-        
-        return insertion_positions
-    
-    def insert_insertion_sequence(self, row):
-        
-        """
-        Adds missing insertion sequence into partial insertions
-        found in plateaus
-        """
-        
-        return str(
-            row["Plateau_sequence"][:row["Insertion_location"]] + 
-            "(+" + row["Insertion_sequence"] + ")" + 
-            row["Plateau_sequence"][row["Insertion_location"]:]
-        )
-        
-    def run_pridict(self, config):
-        
-        #subprocess.run([
-        #    "python",
-        #    "pridict_pegRNA_design.py",
-        #    "batch",
-        #    "--input-fname",
-        #    pridict_input_path,
-        #    "--output-fname batchseqs",
-        #    config.pridict_output_path
-        #])
-        
-        subprocess.run([
-            
-            "singularity",
-            "exec",
-            "--bind",
-            "/lustre",
-            config.pridict_image_path,
-            config.pridict_path,
-            "batch",
-            "--input-fname",
-            config.results_directory + "sequences_for_pridict.csv",
-            "--output-fname",
-            config.pridict_output_path
-            
-        ])
-        
-    def read_pridict_output(self, configuration):
-        
-        self.output = pd.read_csv(configuration.pridict_output_path)
-        
-    def clean_pridict_output(self):
-        
-        self.output = self.output.drop([["Original_sequences",
-                                         "Edited_sequences"]],
-                                       axis = 1)
-        self.output = self.output[self.output["Editing_Position"] >= 10]
-        self.output = self.output.sort_values(
-            "PRIDICT_editing_Score_deep", 
-            ascending = False
-        ).reset_index()
+            print(plateaus)
